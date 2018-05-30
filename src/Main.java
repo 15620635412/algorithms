28de49a97d4d86ad438fd7f228d8b9899aa360b4
wyk <1041686792@qq.com>
2018-05-30 11:06:24 +0800

    测试

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        /*InputStreamReader isr = new InputStreamReader(System.in, "UTF-8");
+        BufferedReader br = new BufferedReader(isr);
+        System.out.println(br.readLine());*/
+        int[] arr = new int[10];
+        System.out.println(arr[1]);
+    }
+}
